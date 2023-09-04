d29724b004f5834a914f286446fa0a25a252e164
IvanHromenko <ivangromenko.1003@gmail.com>
2023-09-04 21:18:31 +0300

    DbContext + Get all & Get by id

---
+++ Store.Services.CouponAPI/Data/AppDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Store.Services.CouponAPI.Models;
+
+namespace Store.Services.CouponAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        { 
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                CouponId = 1,
+                CouponCode = "SUMMER2023",
+                DiscountAmount = 10,
+                MinAmount = 20
+            });
+
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                CouponId = 2,
+                CouponCode = "10OFF",
+                DiscountAmount = 10,
+                MinAmount = 20
+            });
+        }
+    }
+}
