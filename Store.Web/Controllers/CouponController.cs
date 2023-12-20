94597f9857ea22e4eb44c6b95a7b1bed90dec538
IvanHromenko <ivangromenko.1003@gmail.com>
2023-12-20 22:04:22 +0200

    create coupon func & view

--- Store.Web/Controllers/CouponController.cs
+++ Store.Web/Controllers/CouponController.cs
@@ -15,6 +15,7 @@ namespace Store.Web.Controllers
         public async Task<IActionResult> CouponIndex()
         {
             List<CouponDto>? list = new();
+
             ResponseDto? response = await _couponService.GetAllCouponsAsync();
 
             if (response != null && response.IsSuccess)
@@ -22,6 +23,31 @@ namespace Store.Web.Controllers
                 list = JsonConvert.DeserializeObject<List<CouponDto>>(Convert.ToString(response.Result));
             }
             return View(list);
+        }
+
+        public async Task<IActionResult> CouponCreate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CouponCreate(CouponDto couponDto)
+        {
+            if (ModelState.IsValid) 
+            {
+                ResponseDto? response = await _couponService.CreateCouponAsync(couponDto);
+
+                if(response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(CouponIndex));
+                }
+            }
+            return View(couponDto);
+        }
+
+        public async Task<IActionResult> CouponDelete(int couponId)
+        {
+            return View();
         }
     }
 }
